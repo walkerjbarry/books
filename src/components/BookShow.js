1cb8ba913e3bfd52b0c4a434327632f5717dd2cd
bw_esc13 <barry.walker@esc13.txed.net>
2023-04-12 10:18:39 -0500

    added random pictures, as well as the ability to edit book titles.  First iteration of the app is complete.  The app will be refactored to be persistent, using JSON Server in the next phase of development.

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,13 +1,35 @@
-function BookShow({ book, onDelete }) {
-    const handleClick = () => {
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete, onEdit }) {
+
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
+    };
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    };
+
+    let content = <h3>{book.title}</h3>
+    if (showEdit) {
+        content = <BookEdit onSubmit={handleSubmit} book={book} />;
     };
 
     return (
         <div className="book-show">
-            {book.title}
+            <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+            <div>{content}</div>
             <div className="actions">
-                <button className="delete" onClick={handleClick}>Delete</button>
+                <button className="edit" onClick={handleEditClick}>Edit</button>
+                <button className="delete" onClick={handleDeleteClick}>Delete</button>
             </div>
     </div>
       
