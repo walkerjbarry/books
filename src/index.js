636d320aa8c136d73bc0a2c7153a08d9a5c34e58
bw_esc13 <barry.walker@esc13.txed.net>
2023-05-03 07:49:27 -0500

    Refactored to introduce Context into the app, created, utilized BookContext, tested with sample value, and confirmed functionality.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BooksContext from './context/books';
+
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+    <BooksContext.Provider value={5}>
+        <App />
+    </BooksContext.Provider>
+    // ONLY the component wrapped  / contained within context.provider 
+    //can access the value prop that is being passed
+    // in this case, <App /> (and it's children) is (are) the only one(s) that can access the value 5
+  );
